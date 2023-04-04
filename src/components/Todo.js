e9b6d456ea445852081fbcd6518b41aafe5f3ad2
Elisabeth Gross <elisabethgross568@gmail.com>
2023-04-04 19:39:33 -0400

    updates patch call with action

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -52,7 +52,7 @@ function Todo() {
   }, []);
 
   function handleChange(e, id) {
-    const checked = e.target.checked
+    const action = !!e.target.checked
     const url = 'https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/patch/'
 
     fetch(`${url}${id}`, {
@@ -61,7 +61,7 @@ function Todo() {
         "X-Api-Key": "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c"
       },
       body: JSON.stringify({
-        'isComplete': checked
+        'isComplete': action
       })
     })
     .then(() => {
